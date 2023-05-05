44b539b3491682938df86c16e50346ec8c42ff25
Alexander Filippov <92172720+AlexFGeeR@users.noreply.github.com>
2023-05-05 14:16:34 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const BASE_URL = 'https://academy.directlinedev.com';
+
+const createCheckbox = (check) => {
+    return `
+    <label class="checkbox">
+        <input class="checkbox__input" id="${check.id}" type="checkbox">
+        <span class="checkbox__mark" style="border-color: ${check.color}"></span>
+    </label>
+    `
+}
+
+const renderCheckbox = (data) => {
+    for (let check of data) {
+        document.querySelector('.tags-wrapper').insertAdjacentHTML('beforeend', createCheckbox(check))
+    }
+}
+
+// Получение тегов
+(async function() {
+    sendReq({
+        url: '/api/tags',
+        headers: {'Content-Type': 'application/json'}
+    })
+    .then(res => {
+        if (res.ok) {
+            return res.json();
+        }
+    })
+    .then(res => {
+        console.log(res);
+        renderCheckbox(res.data);
+    })
+    .catch(err => {
+        if (err._message) {
+            // Todo Вызов окна под ошибку.
+        }
+    })
+})();
+
+function sendReq({url, method = 'GET', headers, body = null}) {
+    return fetch(BASE_URL + url, {
+        method,
+        headers,
+        body
+    });
+}
